a3109d8549a752cb55779c43bc2bb246bf500d63
Allisson Gonçalves da Silva <allissondasilva29@gmail.com>
2025-08-05 22:33:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é uma bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua Inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em Ingles é Hi'
+)
